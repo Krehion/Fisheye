a1121f97da8639b9c2ea7bd10d901e176155f838
Krehion <krehion@hotmail.com>
2024-04-09 14:58:48 +0200

    [Style] style homepage

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,37 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/Photographers_ID/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        // create all needed elements
+        const article = document.createElement('article');
+        const link = document.createElement('a');
+        const imgContainer = document.createElement('div')
+        imgContainer.setAttribute("class", "imgContainer")
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute("alt", "")
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const localization = document.createElement('p');
+        localization.textContent = city + ', ' + country;
+        localization.setAttribute("class", "localization")
+        const citation = document.createElement('p');
+        citation.textContent = tagline;
+        citation.setAttribute("class", "citation")
+        const rate = document.createElement('p');
+        rate.textContent = price + "€/jour";
+        rate.setAttribute("class", "rate")
+
+        // articulate elements in order
+        article.appendChild(link);
+        link.appendChild(imgContainer);
+        imgContainer.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(localization);
+        article.appendChild(citation);
+        article.appendChild(rate);
         return (article);
     }
     return { name, picture, getUserCardDOM }
