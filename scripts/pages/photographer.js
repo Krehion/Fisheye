77c38e7cfed5ae9000c9ad4d10b1deb176a92c62
Krehion <krehion@hotmail.com>
2024-04-24 14:33:05 +0200

    [Feat] add info section on photographer page

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,28 @@
-//Mettre le code JavaScript lié à la page photographer.html
+class PhotographerPage {
+    constructor() {
+        this.$photographerInfo = document.querySelector("#main");
+        this.photographerApi = new PhotographerApi('../../data/photographers.json');
+    }
+
+    async init() {
+        // Fetch photographer data from the API
+        const photographersData = await this.photographerApi.getPhotographers();
+        const myPhotographer = photographersData.filter(selectId);
+        function selectId(photographer) {
+            return photographer.id === 243; // à filtrer sur searchparams au lieu de 243
+        }
+
+        // Generate photographer page content
+        myPhotographer
+            .map(photographer => new Photographer(photographer)) // à retirer : .map et .forEach : ce n'est pas un tableau. Créer 1 seul modèle et 1 seul template à partir des données dans myPhotographer.  
+            .forEach(photographer => {
+                const template = new PhotographerProfile(photographer);
+                this.$photographerInfo.appendChild(
+                    template.createPhotographerProfile()
+                );
+            });
+    }
+}
+
+const photographerPage = new PhotographerPage();
+photographerPage.init();
---
+++ scripts/templates/photographerProfile.js
@@ -0,0 +1,32 @@
+class PhotographerProfile {
+    constructor(photographer) {
+        this._photographer = photographer
+    }
+
+    createPhotographerProfile() {
+        const $photographerInfo = document.createElement('section');
+        $photographerInfo.classList.add('photographer-info-wrapper')
+
+        const infoContent = `
+        <div id="photographer-info-txt">
+            <h1>${this._photographer.name}</h1>
+            <div>
+                <p class="info-localization">${this._photographer.city}, ${this._photographer.country}</p>
+                <p class="info-tagline">${this._photographer.tagline}</p>
+            </div>
+        </div>
+        <div id="photographer-info-contact">
+          <button class="contact_button" onclick="displayModal()">Contactez-moi</button>
+        </div>
+        <div id="photographer-info-img">
+            <div class="img-container">
+                <img src="${this._photographer.portrait}" alt="" />
+            </div>
+        </div>
+        `
+        
+        $photographerInfo.innerHTML = infoContent
+
+        return $photographerInfo
+    }
+}
