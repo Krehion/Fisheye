45e42c8395f7367fff544dbf9aa89a72c4fae56f
Krehion <krehion@hotmail.com>
2024-05-20 17:55:26 +0200

    [Refactor] divide photographer page creation into specific functions, clean syntax, add comments

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,32 +1,101 @@
 class PhotographerPage {
     constructor() {
-        this.$photographerInfo = document.querySelector("#photographer-profile");
-        this.$photographerGallery = document.querySelector("#gallery");
-        this.$main = document.querySelector("#main");
-        this.$modalTxt = document.querySelector(".modal-header-txt")
+        this.photographerInfo = document.querySelector("#photographer-profile");
+        this.photographerGallery = document.querySelector("#gallery");
+        this.main = document.querySelector("#main");
+        this.modalTxt = document.querySelector(".modal-header-txt");
         this.photographerApi = new PhotographerApi('../../data/photographers.json');
-    }
-
-    selectId(photographer) {
-        return photographer.id === id;
     }
     
     async init() {
         let id = parseInt(new URL(document.location).searchParams.get("id"));
-        // Fetch photographer data from the API
-        const photographersData = await this.photographerApi.getPhotographers();
-        const myPhotographers = photographersData.filter(selectId);
-        
-        
-        
 
-        const myPhotographer = myPhotographers[0]; // get the (only) element from generated array
+        // Create profile section
+        const photographerObj = await this.getPhotographer(id);
+        this.setProfile(photographerObj);
 
-        const photographerObj = new Photographer(myPhotographer); // photographerObj is an object
+        // Create gallery section
+        const galleryData = await this.photographerApi.getGallery(id);
+        this.setGallery(galleryData);
+
+        // Handle gallery sorting by
+        this.handleSort(galleryData);
+
+        // Fetch likes and rate data
+        const templateLikes = new PhotographerLikesRate(photographerObj);
+
+        // Generate likes and rate box
+        this.main.appendChild(
+            templateLikes.createPhotographerLikesRate()
+        );
+    }
+
+    handleSort(galleryData) {
+        const currentFilter = document.querySelector("#current_filter");
+        currentFilter.addEventListener("change", function(e){
+            e.preventDefault();
+
+            // Trigger the right sorting corresponding to #current_filter
+            let newGalleryData;
+            if (currentFilter.innerText == "Titre") {
+                newGalleryData = this.sortByTitle(galleryData);
+            } else if (currentFilter.innerText == "Popularité") {
+                newGalleryData = this.sortByPop(galleryData);
+            } else if (currentFilter.innerText == "Date") {
+                newGalleryData = this.sortByDate(galleryData);
+            }
+
+            // Empty current gallery
+            this.dropGallery();
+
+            // Generate new gallery
+            this.setGallery(newGalleryData);
+        }.bind(this)); // Use bind to keep the context of 'this'
+    }
+    
+// TODO
+    sortByPop(medias) {
+        const mediaOrdered = Array.from(medias);
+        mediaOrdered.sort(function (a, b) {
+            return a.likes - b.likes;
+        });
+        console.log(mediaOrdered);
+        // Update gallery with sorted data
+        this.dropGallery();
+        this.setGallery(mediaOrdered);
+    }
+
+// TODO
+    sortByDate() {
+        // Sorting logic here
+        }
+    
+// TODO
+    sortByTitle() {
+        // Sorting logic here
+    }
+
+    // Empty gallery (used to display new sorted one)
+    dropGallery() {
+        this.photographerGallery.innerHTML = "";
+    }
+
+    // Generate gallery
+    setGallery(galleryData) {
+        galleryData
+            .map(mediaData => MediaFactory.createMedia(mediaData))
+            .forEach(media => {
+                const template = new PhotographerMedias(media);
+                this.photographerGallery.appendChild(
+                    template.createMediaGallery()
+                );
+            });
+    }
+
+    // Generate profile section
+    setProfile(photographerObj) {
         const template = new PhotographerProfile(photographerObj);
-
-        // Generate profile section
-        this.$photographerInfo.appendChild(
+        this.photographerInfo.appendChild(
             template.createPhotographerProfile()
         );
 
@@ -34,34 +103,23 @@ class PhotographerPage {
         modal();
 
         // Add photographer's name in contact form
-        this.$modalTxt.appendChild(
+        this.modalTxt.appendChild(
             template.getPhotographerName()
         );
+    }
 
-        // Fetch gallery data
-        const galleryData = await this.photographerApi.getGallery(id);
+    async getPhotographer(id) {
+        // Fetch photographer data from the API
+        const photographersData = await this.photographerApi.getPhotographers();
+        const myPhotographers = photographersData.filter(function(photographer) {
+            return photographer.id === id;
+        });
 
-        // Generate gallery : à mettre dans une function -> pouvoir supprimer contenu et rappeler la function pour le tri
-        galleryData
-            .map(mediaData => MediaFactory.createMedia(mediaData))
-            .forEach(media => {
-                const template = new PhotographerMedias(media);
-                this.$photographerGallery.appendChild(
-                    template.createMediaGallery()
-                );
-            });
+        // get the (only) element from generated array
+        const myPhotographerId = myPhotographers[0];
         
-
-        // Handle gallery sorting by
-        sortBy();
-
-        // Fetch likes and rate data
-        const templateLikes = new PhotographerLikesRate(photographerObj);
-
-        // Generate likes and rate box
-        this.$main.appendChild(
-            templateLikes.createPhotographerLikesRate()
-        );
+        // photographerObj is an object
+        return new Photographer(myPhotographerId);
     }
 }
 
