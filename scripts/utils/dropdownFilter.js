12903e2000699c525fa379a651936b755fb20404
Krehion <krehion@hotmail.com>
2024-06-14 21:38:57 +0200

    [Fix] dropdown filter z-index behavior on click/focus

--- scripts/utils/dropdownFilter.js
+++ scripts/utils/dropdownFilter.js
@@ -1,5 +1,6 @@
 const currentFilter = document.querySelector("#current_filter");
 const dropdownContent = document.querySelector(".dropdown_content");
+const btnDrop = document.querySelector(".btn_drop");
 
 const btnSortPop = document.querySelector(".sort-by-pop");
 const btnSortDate = document.querySelector(".sort-by-date");
@@ -23,20 +24,29 @@ function reorderList(selectedItem) {
 btnSortPop.addEventListener("click", function () {
 	currentFilter.innerHTML = "Popularité";
 	reorderList(btnSortPop);
+	if (btnDrop.style.zIndex === "-10") {
+		btnDrop.style.zIndex = "100";
+	}
 });
 
 btnSortDate.addEventListener("click", function () {
 	currentFilter.innerHTML = "Date";
 	reorderList(btnSortDate);
+	if (btnDrop.style.zIndex === "-10") {
+		btnDrop.style.zIndex = "100";
+	}
 });
 
 btnSortTitle.addEventListener("click", function () {
 	currentFilter.innerHTML = "Titre";
 	reorderList(btnSortTitle);
+	if (btnDrop.style.zIndex === "-10") {
+		btnDrop.style.zIndex = "100";
+	}
 });
 
 // Keyboard navigation
-const btnDrop = document.querySelector(".btn_drop");
+
 // Make .btn_drop unfocusable
 btnDrop.setAttribute("tabindex", "-1");
 // TODO : display .dropdown_content on top of .btn_drop & make .dropdown height 150px when a sort-by button is focused
